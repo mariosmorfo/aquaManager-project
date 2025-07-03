c56a9430fb196031087224fd707935574d97a870
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-03 17:36:38 +0300

    feat: import cage managment component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CageManagement } from './components/cage-management/cage-management';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CageManagement],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
