5ea5addb8b03f1e0b1c674f65d3180ee18d53189
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-03 20:31:25 +0300

    feat: create paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CageManagement } from './components/cage-management/cage-management';
+import { FishStocking } from './components/fish-stocking/fish-stocking';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'cage-management', component: CageManagement},
+  {path: 'fish-stocking', component: FishStocking}
+];
