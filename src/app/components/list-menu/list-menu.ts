e78a57b5019e1da35b7e37d34a5dae8b9701dfa1
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-03 20:34:36 +0300

    feat: create text and linkName for the components

--- src/app/components/list-menu/list-menu.ts
+++ src/app/components/list-menu/list-menu.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-list-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './list-menu.html',
   styleUrl: './list-menu.css'
 })
 export class ListMenu {
-
+  menu = [
+    {text: "Cage Management", linkName:'cage-management'},
+    {text: "Fish Stocking", linkName: 'fish-stocking'}
+  ]
 }
