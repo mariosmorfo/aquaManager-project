595bb2b56306812ed8994524001c11fd4bc81822
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-04 14:46:29 +0300

    feat: Create add fucntion for cages

--- src/app/components/cage-management/cage-management.ts
+++ src/app/components/cage-management/cage-management.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cage-management',
-  imports: [],
+  standalone: true,
+  imports: [MatListModule, MatButtonModule, CommonModule],
   templateUrl: './cage-management.html',
   styleUrl: './cage-management.css'
 })
 export class CageManagement {
+    cageList: string[] = ['Cage Name 1'];
 
+ addCage() {
+  const nextNumber = this.cageList.length + 1;
+  const newName = `Cage Name ${nextNumber}`;
+  console.log('Adding:', newName);
+  this.cageList.push(newName);
+}
 }
