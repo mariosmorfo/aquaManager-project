e9f8c76e288e123c19f8c3dfe64dd1bd8469c6cb
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-04 13:22:31 +0300

    create cage management component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { ListMenu } from "./components/list-menu/list-menu";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ListMenu],
+  imports: [RouterOutlet,  ListMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
