70eb0f414ca8534edb6074067f265a2d03ce3190
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-06 01:57:19 +0300

    feat: create submit stocking function

--- src/app/components/fish-stocking/fish-stocking.ts
+++ src/app/components/fish-stocking/fish-stocking.ts
@@ -1,58 +1,56 @@
 import { Component } from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import { CageService } from '../../services/cage';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
-import {  FormControl } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { CageService } from '../../services/cage';
 
-/**
- * FishStocking component allows the user to select a date, cage, and number of fish.
- * Uses Reactive Forms to collect input and interacts with CageService to load cage options.
- */
 @Component({
   selector: 'app-fish-stocking',
   standalone: true,
-  imports: [MatDatepickerModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, CommonModule, ReactiveFormsModule, FormsModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
+  ],
   templateUrl: './fish-stocking.html',
-    providers: [provideNativeDateAdapter()],
-  styleUrl: './fish-stocking.css'
+  styleUrls: ['./fish-stocking.css']
 })
 export class FishStocking {
-constructor(private cageService: CageService) {}
+  selectedDate: Date | null = null;
+  cages: string[] = [];
+  cageStockingData: { cage: string, fishNumber: number | null }[] = [];
 
- /** Selected cage(s) from the multi-select dropdown */
-selectedCages: string[] = [];
+  displayedColumns = ['cage', 'fishNumber'];
 
-  /** Selected date from the date picker */
-selectedDate: Date | null = null;
+  constructor(private cageService: CageService) {}
 
-/** Form control for the fish number input; initialized to 0 */
-showFishNumber = new FormControl(0);
+  ngOnInit() {
+    this.cages = this.cageService.getCages();
 
-/** List of available cage names loaded from the service */
-cages: string[] = [];
+    this.cageStockingData = this.cages.map(cage => ({
+      cage,
+      fishNumber: null
+    }));
+  }
 
- /**
-   * Lifecycle hook that runs on component init.
-   * Loads the list of cage names from CageService.
-   */
-ngOnInit(){
-  console.log('Loaded cages:', this.cages);
-  this.cages = this.cageService.getCages();
-}
-printStockingData() {
-  console.log(' Fish Stocking Info:');
-  console.log('Date:', this.selectedDate);
-  console.log('Selected Cage:', this.selectedCages);
-  console.log('Fish Number:', this.showFishNumber.value);
-}
+  submitStocking() {
+    const result = {
+      date: this.selectedDate,
+      records: this.cageStockingData.filter(row => row.fishNumber !== null)
+    };
 
-  
+    console.log(' Stocking Submitted:', result);
+  }
 }
