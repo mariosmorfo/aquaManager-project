f04b6921df6339bd43aeb1b44608fde3f694fdcc
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-06 01:43:50 +0300

    refactor: delete function for loadStorage

--- src/app/services/cage.ts
+++ src/app/services/cage.ts
@@ -4,38 +4,22 @@ import { Injectable } from '@angular/core';
 export class CageService {
   private cages: string[] = [];
 
-   constructor() {
-    this.loadFromStorage(); 
-  }
-
-  private saveToStorage(): void {
-    localStorage.setItem('cages', JSON.stringify(this.cages));
-  }
-
-  private loadFromStorage(): void {
-    const stored = localStorage.getItem('cages');
-    this.cages = stored ? JSON.parse(stored) : [];
-  }
 
   getCages(): string[] {
-    this.loadFromStorage();
     return this.cages;
   }
 
   addCage(name: string) {
     this.cages.push(name);
-    this.saveToStorage()
   }
 
   deleteCage(index: number) {
     this.cages.splice(index, 1);
-    this.saveToStorage();
   }
 
   renameCage(index: number, newName: string): void {
   if (index >= 0 && index < this.cages.length) {
     this.cages[index] = newName;
-    this.saveToStorage()
   }
 }
 
