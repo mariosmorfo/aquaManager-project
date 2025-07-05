3593b170593e960a05aa053a3343feacae86e129
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-05 18:08:45 +0300

    feat: Create functions add and delete cage with DI from the services

--- src/app/components/cage-management/cage-management.ts
+++ src/app/components/cage-management/cage-management.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { CageService } from '../../services/cage';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cage-management',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, CommonModule],
+  imports: [CommonModule, MatListModule, MatButtonModule, FormsModule],
   templateUrl: './cage-management.html',
-  styleUrl: './cage-management.css'
+  styleUrls: ['./cage-management.css']
 })
 export class CageManagement {
-    cageList: string[] = ['Cage Name 1'];
 
- addCage() {
-  const nextNumber = this.cageList.length + 1;
-  const newName = `Cage Name ${nextNumber}`;
-  console.log('Adding:', newName);
-  this.cageList.push(newName);
-}
+  editIndexMap: { [index: number]: boolean } = {};
+  editValues: { [index: number]: string } = {};
+
+  constructor(private cageService: CageService) {}
+
+  get cageList(): string[] {
+    return this.cageService.getCages();
+  }
+
+  addCage(): void {
+    const name = `Cage Name ${this.cageList.length + 1}`;
+    this.cageService.addCage(name);
+  }
+
+  deleteCage(index: number): void {
+    this.cageService.deleteCage(index);
+    delete this.editIndexMap[index];
+    delete this.editValues[index];
+  }
+
+ 
 }
