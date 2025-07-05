e3a5ee72ec481ae56b8eb659e2caeab72f2af6f9
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-05 18:14:34 +0300

    feat: Loads the cage names from the service and stores them in cages to be used in the UI

--- src/app/components/fish-stocking/fish-stocking.ts
+++ src/app/components/fish-stocking/fish-stocking.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { CageService } from '../../services/cage';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fish-stocking',
-  imports: [],
+  standalone: true,
+  imports: [MatDatepickerModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatListModule, FormsModule, CommonModule],
   templateUrl: './fish-stocking.html',
+    providers: [provideNativeDateAdapter()],
   styleUrl: './fish-stocking.css'
 })
 export class FishStocking {
 
+selectedCages: string[] = [];
+// selectedDate: Date | null = null;
+constructor(private cageService: CageService) {}
+
+cages: string[] = [];
+
+ngOnInit(){
+  //  console.log('Loaded cages:', this.cages);
+  this.cages = this.cageService.getCages();
+}
+
+  
 }
