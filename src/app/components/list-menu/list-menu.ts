0d307b7c5e6b498fd3351ed0df4717fc78a34b76
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-07 00:34:06 +0300

    feat: add doc comments

--- src/app/components/list-menu/list-menu.ts
+++ src/app/components/list-menu/list-menu.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+/**
+ * ListMenu component
+ * 
+ * This component renders a list menu with navigation links for various 
+ * management functions in the application such as cage management, 
+ * fish stocking, and mortality registration.
+ */
 @Component({
   selector: 'app-list-menu',
   imports: [RouterLink, RouterLinkActive],
